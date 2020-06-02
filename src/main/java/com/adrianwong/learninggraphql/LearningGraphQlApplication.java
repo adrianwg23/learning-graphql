d08976557bb55ea52a52b9d7a19842f175ab9bae
adrianwg23 <adrianwg23@hotmail.com>
2020-06-02 18:42:14 -0400

    init project

---
+++ src/main/java/com/adrianwong/learninggraphql/LearningGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.adrianwong.learninggraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningGraphQlApplication.class, args);
+	}
+
+}
