59a006d5be6f4a29bac9f250756d94763d8c6e19
adrianwg23 <adrianwg23@hotmail.com>
2020-06-02 20:11:52 -0400

    create ImageController

--- src/main/java/com/adrianwong/learninggraphql/web/ImageController.java
+++ src/main/java/com/adrianwong/learninggraphql/web/ImageController.java
@@ -1,4 +1,16 @@
 package com.adrianwong.learninggraphql.web;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/images")
+@RestController
 public class ImageController {
+
+    @PostMapping
+    public void getImages(@RequestBody String query) {
+
+    }
 }
