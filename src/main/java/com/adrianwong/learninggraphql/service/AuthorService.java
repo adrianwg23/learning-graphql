f0ff71f764d833f24f7196756d03571bb41a7bbf
adrianwg23 <adrianwg23@hotmail.com>
2020-06-04 09:06:50 -0400

    add service classes

--- src/main/java/com/adrianwong/learninggraphql/service/AuthorService.java
+++ src/main/java/com/adrianwong/learninggraphql/service/AuthorService.java
@@ -1,4 +1,7 @@
 package com.adrianwong.learninggraphql.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthorService {
 }
--- src/main/java/com/adrianwong/learninggraphql/service/BookService.java
+++ src/main/java/com/adrianwong/learninggraphql/service/BookService.java
@@ -1,4 +1,17 @@
 package com.adrianwong.learninggraphql.service;
 
+import com.adrianwong.learninggraphql.repository.AuthorRepository;
+import com.adrianwong.learninggraphql.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+
+    public BookService(BookRepository bookRepository, AuthorRepository authorRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+    }
 }
